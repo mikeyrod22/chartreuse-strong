fce4b18ea949761d6b978ffbe58a61dd00575676
Michael Rodriguez <mikeyrod22@gmail.com>
2021-03-04 15:28:17 -0600

    wrote passwordLengthPrompt() function

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,29 @@
 // Assignment code here
 
+// returns valid password length
+function passwordLengthPrompt() {
+  let passwordLength = window.prompt('How many characters would you like your password to have? (password must be between 8 and 128 characters long)');
+  passwordLength = Number(passwordLength);
+  
+  // valid input
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    console.log(`password length: ${passwordLength}`);
+  
+  // invalid input
+  } else {
+    window.alert('Invalid input. Please try again');
+    passwordLengthPrompt();
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
 function writePassword() {
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
-
-  // passwordText.value = password;
-
-  const passwordLength = window.prompt('How many characters would you like your password to have? (password must be between 8 and 128 characters long)');
+  passwordLengthPrompt();
+  // (starter) var password = generatePassword();
+  // (starter) var passwordText = document.querySelector("#password");
+  // (starter) passwordText.value = password;
 }
 
 // Add event listener to generate button
